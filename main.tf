3655e7f0478a0ff7db5f1c19608c27f1017abdf1
Jimmy Nguyen <jimmynguyenhnn@gmail.com>
2024-11-08 02:20:52 -0800

    App Changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2" # Replace with your preferred region
 }
 
 # Create Elastic Beanstalk application
@@ -7,11 +7,19 @@ resource "aws_elastic_beanstalk_application" "app" {
   name = "sample-nodejs-app"
 }
 
+# Create Elastic Beanstalk application version
+resource "aws_elastic_beanstalk_application_version" "app_version" {
+  name        = "v1"                           # Version label
+  application = aws_elastic_beanstalk_application.app.name
+  bucket      = aws_s3_bucket.logs.id          # Reference your S3 bucket
+  key         = "app.zip"                      # Path to the uploaded app in S3
+}
+
 # Create Elastic Beanstalk environment
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "sample-nodejs-env"
   application         = aws_elastic_beanstalk_application.app.name
-  version_label       = aws_elastic_beanstalk_application_version.app_version.label
+  version_label       = aws_elastic_beanstalk_application_version.app_version.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.2 running Node.js 14"
 
   setting {
@@ -25,12 +33,4 @@ resource "aws_elastic_beanstalk_environment" "env" {
     name      = "NODE_ENV"
     value     = "production"
   }
-}
-
-# Upload and deploy application version
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  name        = "v1"
-  application = aws_elastic_beanstalk_application.app.name
-  bucket      = aws_s3_bucket.logs.id
-  key         = "app.zip"
 }
--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "logs" {
+resource "aws_s3_object" "logs" {
   bucket = "sample-app-bucket-${random_string.suffix.result}"
 }
 
-resource "aws_s3_bucket_object" "app_zip" {
+resource "aws_s3_object" "app_zip" {
   bucket = aws_s3_bucket.logs.id
   key    = "app.zip"
   source = "app/app.zip" # Path to the app zip file
